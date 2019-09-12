78b7c58cdd45f210be6f5b8238175ff01881eaf1
miferreiro <miguel.ferreiro.diaz@gmail.com>
2019-09-12 09:16:37 +0200

    Added new Plot class
    This new classes implements different types of plot.

---
+++ source/clustering/plot/BinaryPlot.R
@@ -0,0 +1,28 @@
+library("R6")
+BinaryPlot <- R6Class(
+  classname = "BinaryPlot",
+  inherit = Plot,
+  portable = TRUE,
+  public = list(
+    initialize = function(name = "BinaryPlot") {
+      super$initialize(name = name)
+    },
+    plot = function(summary, ...) {
+      if (!is.data.frame(summary)) {
+        stop("[", super$getName(), "][ERROR] summary parameter must be defined as 'data.frame' type")
+      }
+      min <- data.frame(x = summary[which.min(summary[, 2]), ][, 1],y = min(summary[, 2]))
+      max <- data.frame(x = summary[which.max(summary[, 2]), ][, 1],y = max(summary[, 2]))
+      ggplot(summary, aes(k, dispersion)) + geom_point(aes(color = dispersion), position = position_jitter()) + 
+        scale_color_continuous(name = "",low = "blue", high = "red", guide = FALSE ) + 
+        geom_text_repel(aes(x, y, label = sprintf("%s", format(min$y, digits = 2, scientific = TRUE))), 
+                        min, hjust = 0.5, vjust = 0, point.padding = 0.25, color = 'blue', size = 3 ) +
+        geom_text_repel(aes(x, y, label = sprintf("%s", format(max$y,digits = 2, scientific = TRUE))), 
+                        max, hjust = 0.5, vjust = 1, point.padding = 0.25, color = 'red', size = 3) + 
+        scale_y_continuous(name = "Dispersion (represented as logaritmic scale)", trans = "log2", breaks = c(min$y, max$y)) + 
+        scale_x_continuous(name = "Number of clusters", breaks = seq(from = 2, to = nrow(summary) + 1)) + 
+        labs(title = "Binary Data") + theme_light() + theme(axis.text.x = element_text(angle = 90, hjust = 0.5))
+    
+    }
+  )
+)
---
+++ source/clustering/plot/KendallPlot.R
@@ -0,0 +1,22 @@
+library("R6")
+KendallPlot <- R6Class(
+  classname = "KendallPlot",
+  inherit = Plot,
+  portable = TRUE,
+  public = list(
+    initialize = function(name = "KendallPlot") {
+      super$initialize(name = name)
+    },
+    plot = function(summary, ...) {
+      ggplot(data = summary, aes(x = interval, y = value)) +
+        geom_bar(stat = "identity", fill = "steelblue") +
+        geom_text_repel(aes(label = summary$value), size = 4, hjust = 1.6, color = "white" ) +
+        coord_flip() + 
+        scale_y_continuous(expand = c(0, 0),
+                           limits = c(0, max(summary$value) * 1.2)) +  
+        geom_hline(yintercept = max(summary$value), linetype = "dashed", color = "black") + 
+        labs(title = "Unbinary Data", x = "Class", y = "Mean Tau Value") + 
+        theme_light()
+    }
+  )
+)
